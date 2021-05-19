dfaedbd1bff44abbd10e74baab02d4ad3aef36d9
Akarshit Mahendru <akarshit.mahendru@jaarvis.com>
2021-05-20 02:14:16 +0530

    advanced search api

--- dalyne/import_export/serializers.py
+++ dalyne/import_export/serializers.py
@@ -7,6 +7,22 @@ file_choice_field = [
     ("export", "Export")
 ]
 
+search_choice_field = [
+    ("hs_code", "HS code"),
+    ("hs_4_digit_code", "HS 4 digits code"),
+    ("product", "Product"),
+    ("exporter_name", "Exporter Name"),
+    ("importer_name", "Exporter Name"),
+    ("iec_code", "IEC code")
+]
+
+operator_field = [
+    ("contains", "Contains"),
+    ("exact", "Exact"),
+    ("partial", "partial")
+]
+
+
 class PlansListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plans
@@ -28,16 +44,17 @@ class ExportViewSerializer(serializers.ModelSerializer):
         model = ExportTable
         fields = '__all__'
 
+
 class ProductMasterSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductMaster
-        fields = ('id','hs_code','description')
+        fields = ('id', 'hs_code', 'description')
 
 
 class CompanyMasterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyMaster
-        fields = ('id','iec_code','name')
+        fields = ('id', 'iec_code', 'name')
 
 
 class ProductImportSerializer(serializers.Serializer):
@@ -95,5 +112,53 @@ class CountryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = CountryMaster
         fields = ('id', 'name')
+
+
+class ImporterDataFilterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ImportTable
+        fields = ('TWO_DIGIT', 'FOUR_DIGIT', 'RITC_DISCRIPTION', 'UQC', 'QUANTITY', 'BE_NO', 'IMPORTER_NAME',
+                  'EXPORTER_NAME', 'COUNTRY_OF_ORIGIN', 'PORT_OF_LOADING', 'PORT_OF_DISCHARGE', 'MODE_OF_PORT',
+                  'PORT_CODE', 'MONTH', 'YEAR')
+
+
+class ExporterDataFilterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ExportTable
+        fields = ('TWO_DIGIT', 'FOUR_DIGIT', 'RITC_DISCRIPTION', 'UQC', 'QUANTITY', 'SB_NO', 'IMPORTER_NAME',
+                  'EXPORTER_NAME', 'COUNTRY_OF_ORIGIN', 'PORT_OF_LOADING', 'PORT_OF_DISCHARGE', 'MODE_OF_PORT',
+                  'PORT_CODE', 'MONTH', 'YEAR')
+
+
+class FilterDataSerializer(serializers.Serializer):
+    data_type = fields.ChoiceField(
+        choices=file_choice_field,
+        required=True
+    )
+    country = serializers.CharField(
+        required=True,
+        allow_null=False
+    )
+    start_date = serializers.DateField(
+        required=True,
+        input_formats=["%Y-%m-%d"]
+    )
+    end_date = serializers.DateField(
+        required=True,
+        input_formats=["%Y-%m-%d"]
+    )
+    search_field = fields.ChoiceField(
+        choices=search_choice_field,
+        required=True
+    )
+    operator_type = fields.ChoiceField(
+        choices=operator_field,
+        required=True
+    )
+    search_value = serializers.CharField(
+        required=True
+    )
 
 
