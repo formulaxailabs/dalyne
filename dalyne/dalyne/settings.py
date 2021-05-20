74bc43cdefdbc9df3e2a0ae11733e04bc5c3ece8
abhisek6977 <abhisek.singh@secureyourinbox.com>
2021-05-20 11:39:02 +0530

    fix

--- dalyne/dalyne/settings.py
+++ dalyne/dalyne/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ENV_CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
 if not ENV_CELERY_BROKER_URL or  len(ENV_CELERY_BROKER_URL) == 0:
