44d67225074043a32a1e472d36ce0abab8fad7a5
Akarshit Mahendru <akarshit.mahendru@jaarvis.com>
2021-05-20 14:55:15 +0530

    cors fix

--- dalyne/dalyne/settings.py
+++ dalyne/dalyne/settings.py
@@ -55,9 +55,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -66,6 +66,18 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with'
+]
 
 ROOT_URLCONF = 'dalyne.urls'
 
