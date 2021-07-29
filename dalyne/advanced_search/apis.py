5439d4efdba1b54a4b6ac6c3b608d68a6bc7cd8f
Akarshit Mahendru <akarshit.mahendru@jaarvis.com>
2021-07-29 14:56:01 +0530

    ordered data api

--- dalyne/advanced_search/apis.py
+++ dalyne/advanced_search/apis.py
@@ -172,6 +172,7 @@ class ExportAPIView(views.APIView):
         operation_id="Export Data")
     def post(self, request, *args, **kwargs):
         excel_limit = QUERY_LIMIT
+        tenant = self.request.user.tenant
         requested_qs = RequestedDownloadModel.objects.filter(user_id=self.request.user.id).first()
         try:
             if not requested_qs:
@@ -452,3 +453,137 @@ class ExporterImporterList(generics.ListAPIView):
             queryset = model.objects.filter(COUNTRY__id=country, BE_DATE__gte=start_date,
                                             BE_DATE__lte=end_date).distinct('EXPORTER_NAME')
         return queryset
+
+
+class DownloadMessage(views.APIView):
+    model = RequestedDownloadModel
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+
+class OrderingListingAPI(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (djfilters.DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter,
+                       )
+    ordering_fields = None
+
+    def get_serializer_class(self):
+        search_id = self.request.query_params.get("search_id")
+        search_obj = FilterDataModel.objects.filter(id=search_id).first()
+        if search_obj:
+            if search_obj.data_type == "export":
+                return ExporterDataFilterSerializer
+            elif search_obj.data_type == "import":
+                return ImporterDataFilterSerializer
+        else:
+            return ImporterDataFilterSerializer
+
+    def get_queryset(self):
+        search_id = self.request.query_params.get('search_id')
+        exporter = self.request.query_params.get('exporter', None)
+        importer = self.request.query_params.get('importer', None)
+        uqc = self.request.query_params.get('uqc', None)
+        country_origin = self.request.query_params.get('country', None)
+        port_of_discharge = self.request.query_params.get('port_of_discharge', None)
+        port_of_loading = self.request.query_params.get('port_of_loading', None)
+        mode = self.request.query_params.get('mode', None)
+        port_code = self.request.query_params.get('port_code', None)
+        hs_code = self.request.query_params.get('hs_code', None)
+        description = self.request.query_params.get('description', None)
+        min_qty = self.request.query_params.get('min_qty', None)
+        max_qty = self.request.query_params.get('max_qty', None)
+
+        if search_id:
+            search_obj = FilterDataModel.objects.filter(id=search_id).first()
+            if search_obj:
+                country = search_obj.country.name
+                start_date = search_obj.start_date
+                end_date = search_obj.end_date
+                search_field = search_obj.search_field
+                search_value = search_obj.search_value
+                data_type = search_obj.data_type
+                if data_type == "export":
+                    model = ExportTable
+                    self.ordering_fields = ('PORT_OF_LOADING', 'PORT_OF_DISCHARGE', 'QUANTITY', 'MODE_OF_PORT',
+                                            'EXPORTER_NAME', 'IMPORTER_NAME', 'UQC', 'COUNTRY_OF_ORIGIN', 'PORT_CODE',
+                                            'BE_DATE', 'HS_CODE')
+                    qs = model.objects.filter(COUNTRY__name=country, BE_DATE__gte=start_date,
+                                              BE_DATE__lte=end_date)
+                else:
+                    model = ImportTable
+
+                    self.ordering_fields = ('PORT_OF_LOADING', 'PORT_OF_DISCHARGE', 'QUANTITY', 'MODE_OF_PORT',
+                                            'EXPORTER_NAME', 'IMPORTER_NAME', 'UQC', 'COUNTRY_OF_ORIGIN', 'PORT_CODE',
+                                            'BE_DATE', 'HS_CODE')
+                    qs = model.objects.filter(COUNTRY__name=country, BE_DATE__gte=start_date,
+                                              BE_DATE__lte=end_date)
+                if search_field == "hs_code":
+                    qs = qs.filter(Q(TWO_DIGIT__in=search_value) |
+                                   Q(FOUR_DIGIT__in=search_value) |
+                                   Q(HS_CODE__in=search_value))
+                if search_field == "importer_name":
+                    qs = qs.filter(IMPORTER_NAME__in=search_value)
+                if search_field == "exporter_name":
+                    qs = qs.filter(EXPORTER_NAME__in=search_value)
+                if search_field == "product":
+                    product_qs = ProductMaster.objects.filter(description__in=search_value)
+                    hs_code_list = list()
+                    for obj in product_qs:
+                        hs_code_list.append(obj.hs_code)
+                    qs = qs.filter(Q(TWO_DIGIT__in=hs_code_list) |
+                                   Q(FOUR_DIGIT__in=hs_code_list) |
+                                   Q(HS_CODE__in=hs_code_list))
+                if search_field == "hs_description":
+                    initial_queryset = model.objects.none()
+                    for value in search_value:
+                        initial_queryset = initial_queryset | qs.filter(HS_CODE_DESCRIPTION__icontains=value)
+                    qs = initial_queryset
+                if search_field == "iec_code":
+                    if model == ExportTable:
+                        qs = qs.filter(IEC__in=search_value)
+                    if model == ImportTable:
+                        qs = qs.filter(IMPORTER_ID__in=search_value)
+                if exporter:
+                    if model == ExportTable:
+                        qs = qs.filter(Q(IEC__icontains=exporter) |
+                                       Q(EXPORTER_NAME__icontains=exporter))
+                    if model == ImportTable:
+                        qs = qs.filter(EXPORTER_NAME__icontains=exporter)
+                if importer:
+                    if model == ExportTable:
+                        qs = qs.filter(IMPORTER_NAME__icontains=importer)
+                    if model == ImportTable:
+                        qs = qs.filter(Q(IMPORTER_NAME__icontains=importer) |
+                                       Q(IMPORTER_ID__icontains=importer))
+
+                if uqc:
+                    qs = qs.filter(UQC__icontains=uqc)
+                if country_origin:
+                    qs = qs.filter(COUNTRY_OF_ORIGIN__icontains=country_origin)
+                if port_of_loading:
+                    qs = qs.filter(PORT_OF_LOADING__icontains=port_of_loading)
+                if port_of_discharge:
+                    qs = qs.filter(PORT_OF_DISCHARGE__icontains=port_of_discharge)
+                if mode:
+                    qs = qs.filter(MODE_OF_PORT__icontains=mode)
+                if port_code:
+                    qs = qs.filter(PORT_CODE__icontains=port_code)
+                if description:
+                    qs = qs.filter(HS_CODE_DESCRIPTION__icontains=description)
+                if hs_code:
+                    qs = qs.filter(HS_CODE__icontains=hs_code)
+                if min_qty or max_qty:
+                    if (min_qty and not max_qty) or (max_qty and not min_qty):
+                        raise exceptions.ValidationError("Both min quantity and max quantity are required")
+                    else:
+                        qs = qs.filter(QUANTITY__gte=min_qty, QUANTITY__lte=max_qty)
+                return qs
+            else:
+                return {}
+        else:
+            return {}
+
+
--- dalyne/advanced_search/urls.py
+++ dalyne/advanced_search/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^filtered/data/$', apis.SubFilterListingAPI.as_view(), name='types'),
     url(r'export/shipments/$', apis.ExportAPIView.as_view(), name='shipments'),
-    url(r'exporters/importers/list/$', apis.ExporterImporterList.as_view(), name='name-list')
+    url(r'exporters/importers/list/$', apis.ExporterImporterList.as_view(), name='name-list'),
+    url(r'ordered/data/$', apis.OrderingListingAPI.as_view(), name='ordered')
 
 ]
